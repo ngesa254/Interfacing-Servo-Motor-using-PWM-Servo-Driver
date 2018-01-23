0b234d2881c7ce9f648f791a19f971d4b69e1714
ngesa marvin <marvinngesa@gmail.com>
2018-01-23 06:31:37 +0300

    set servo configurations

--- app/src/main/java/com/droidmarvin/interfacingservomotorusingpwmservodriver/MainActivity.java
+++ app/src/main/java/com/droidmarvin/interfacingservomotorusingpwmservodriver/MainActivity.java
@@ -21,9 +21,13 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
     }
 
+    // Access the servo
     private void setupServo(){
         try {
            mServo = new Servo(PWM_BUS);
+            mServo.setPulseDurationRange(.5, 2.5); // according to your servo's specifications
+            mServo.setAngleRange(0, 180);       // according to your servo's specifications
+            mServo.setEnabled(true);
         }catch (IOException e){
             Log.e(TAG, "Error creating Servo", e);
         }
