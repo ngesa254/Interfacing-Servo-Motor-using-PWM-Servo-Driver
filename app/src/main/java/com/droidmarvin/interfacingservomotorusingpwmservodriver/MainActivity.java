83941ad71facf4351279450bbc792bec718181fc
ngesa marvin <marvinngesa@gmail.com>
2018-01-23 06:21:21 +0300

    Interfacing servo motor with servo motor driver using PMW initial things commit

---
+++ app/src/main/java/com/droidmarvin/interfacingservomotorusingpwmservodriver/MainActivity.java
@@ -0,0 +1,12 @@
+package com.droidmarvin.interfacingservomotorusingpwmservodriver;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
